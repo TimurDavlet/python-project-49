8baa96ef59b202bb6b576ce6dab8708de8e9622f
TimurDavlet <timkoootim@yandex.ru>
2024-08-05 14:54:46 +0300

    4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 from brain_games import cli
 
+
 def hello():
     print('Welcome to the Brain Games!')
+
 
 def main():
     hello()
     cli.welcome_user()
+
 
 if __name__ == '__main__':
     main()
